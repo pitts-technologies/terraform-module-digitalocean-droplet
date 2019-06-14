c31ad5428b928bbb27a1def59613b8642e74af37
pitts-michael <pittsm1@aetna.com>
2019-06-14 11:23:13 -0400

    [droplet module] adding input variables

--- variables.tf
+++ variables.tf
@@ -20,3 +20,51 @@ variable "size" {
   type        = "string"
   default     = "s-1vcpu-1gb"
 }
+
+variable "monitoring" {
+  description = "(Optional) Boolean controlling whether monitoring agent is installed. Defaults to false.(Optional) Boolean controlling whether monitoring agent is installed. Defaults to false."
+  type        = "string"
+  default     = false
+}
+
+variable "backups" {
+  description = "(Optional) Boolean controlling if backups are made. Defaults to false."
+  type        = "string"
+  default     = false
+}
+
+variable "ipv6" {
+  description = "(Optional) Boolean controlling if IPv6 is enabled. Defaults to false."
+  type        = "string"
+  default     = false
+}
+
+variable "private_networking" {
+  description = "(Optional) Boolean controlling if private networks are enabled. Defaults to false."
+  type        = "string"
+  default     = false
+}
+
+variable "ssh_keys" {
+  description = "(Optional) A list of SSH IDs or fingerprints to enable in the format [12345, 123456]. To retrieve this info, use a tool such as curl with the DigitalOcean API, to retrieve them."
+  type        = "list"
+  default     = []
+}
+
+variable "user_data" {
+  description = "(Optional) A string of the desired User Data for the Droplet."
+  type        = "string"
+  default     = ""
+}
+
+variable "volume_ids" {
+  description = "(Optional) A list of the IDs of each block storage volume to be attached to the Droplet."
+  type        = "list"
+  default     = []
+}
+
+variable "tags" {
+  description = "(Optional) A list of the tags to be applied to this Droplet."
+  type        = "list"
+  default     = []
+}
